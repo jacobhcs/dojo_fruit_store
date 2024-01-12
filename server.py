a2bb755c3b9198264d0ca47b42c1712461c6006d
jacobhcs <jaharr1999@gmail.com>
2024-01-12 05:38:27 -0500

    complete

--- server.py
+++ server.py
@@ -8,7 +8,14 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    fn = request.form['first_name']
+    ln = request.form['last_name']
+    sid = request.form['student_id']
+    print(f'Charging {fn} {ln} for {int(strawberry) + int(raspberry) + int(apple)} fruits')
+    return render_template("checkout.html", apple=apple, raspberry=raspberry, strawberry=strawberry, fn=fn, ln=ln, sid=sid)
 
 @app.route('/fruits')         
 def fruits():
